617b5343251f045e534e41085546655ae50edfda
b-aq <bashar.3akleh_92@hotmail.com>
2021-07-26 13:45:51 +0300

    fixed an error

--- lab01/src/component/HornedBeasts.js
+++ lab01/src/component/HornedBeasts.js
@@ -5,20 +5,22 @@ class HornedBeasts extends React.Component {
 
     render() {
         return (
-            <h2>
-                {this.props.title}
-            </h2>
-            <img src= {this.props.img} alt = ''/>
-            <p>
-                {this.props.description}
-            </p>
-            <p>
-                {this.props.keyword}
-            </p>
-            <p>
-                {this.props.horns}
-            </p>
-            
+            <div>
+                <h2>
+                    {this.props.title}
+                </h2>
+                <img src={this.props.img} alt='' />
+                <p>
+                    {this.props.description}
+                </p>
+                <p>
+                    {this.props.keyword}
+                </p>
+                <p>
+                    {this.props.horns}
+                </p>
+            </div>
+
         )
     }
 }
