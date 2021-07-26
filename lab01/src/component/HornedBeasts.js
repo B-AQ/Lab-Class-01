5ea2aa10ef293a46567298e1c5708c9840d167d1
b-aq <bashar.3akleh_92@hotmail.com>
2021-07-26 20:11:47 +0300

    added the react app

--- lab01/src/component/HornedBeasts.js
+++ lab01/src/component/HornedBeasts.js
@@ -1,29 +1,17 @@
-
-import React from 'react';
+import React from "react";
 
 class HornedBeasts extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <h2>
-                    {this.props.title}
-                </h2>
-                <img src={this.props.img} alt='' />
-                <p>
-                    {this.props.description}
-                </p>
-                <p>
-                    {this.props.keyword}
-                </p>
-                <p>
-                    {this.props.horns}
-                </p>
-            </div>
-
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h2>{this.props.title}</h2>
+        <img src={this.props.img} alt="" />
+        <p>{this.props.description}</p>
+        <p>{this.props.keyword}</p>
+        <p>{this.props.horns}</p>
+      </div>
+    );
+  }
 }
 
 export default HornedBeasts;
-
