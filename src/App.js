a4788f863d658740ca93c8d1621067ba203e5e1f
b-aq <bashar.3akleh_92@hotmail.com>
2021-07-28 21:15:29 +0300

    adding some codes to the lab

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Main from './component/Main';
-import Footer from './component/Footer';
-import Header from './component/Header';
+import Main from "./component/Main";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import data from "./component/data.json";
+import ChosenBeasts from "./component/ChosenBeasts";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      image: "",
+      description: "",
+      visibility: false,
+    };
+
+    dataContent = (title, image, descripition, visibility) => {
+      this.setState({
+        title: title,
+        image: image,
+        descripition: descripition,
+        visibility: visibility,
+      });
+    };
+
+    updateVisibility = (visibility) => {
+      this.setState({
+        visibility: visibility,
+      });
+    };
+  }
   render() {
     return (
       <div>
+        <ChosenBeasts
+          title={this.state.title}
+          image={this.state.image}
+          description={this.state.description}
+          visibility={this.state.visibility}
+          hideVisibility={this.updateVisibility}
+        />
         <Header />
-        <Main />
+
+        <Main data={data} dataContent={this.dataContent} />
+
         <Footer />
       </div>
     );
--- src/component/ChosenBeasts.js
+++ src/component/ChosenBeasts.js
@@ -1,30 +1,39 @@
-import React from 'react';
-import Modal from 'react-bootstrap/Modal';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 class Chosen extends React.Component {
-render(){
+  handleClose = () => {
+    this.props.hideVisibility;
+  };
+  render() {
     return (
-        <>
-          <Button variant="primary" onClick={handleShow}>
-            Launch demo modal
-          </Button>
-    
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Modal heading</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-              <Button variant="primary" onClick={handleClose}>
-                Save Changes
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </>
-      );
+      <>
+        <Button variant="primary" onClick={handleClose}>
+          Launch demo modal
+        </Button>
+
+        <Modal show={this.props.visibility} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.props.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img src={this.props.image} alt={this.props.title} />
+            <p>{this.props.descripition}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            {/* <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button> */}
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
 }
 
-}
+export default Chosen;
